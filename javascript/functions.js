6857a5066000c53e9ca94ecd8de973d833bf159b
Jim Sutton <jimsuttonjimsutton@gmail.com>
2015-02-09 13:59:22 -0700

    Add ability to log output in node with helper function

--- javascript/functions.js
+++ javascript/functions.js
@@ -1,13 +1,27 @@
-/*Build a function `my_max()` which takes an array and returns the maximum number. */
-
-function myMax(values) {
-  sorted_values = values.sort(function(a, b) { return a - b });
-  return sorted_values[-1];
+/* Results printer function for logging output to console */
+function printResults(yourFunction, yourParams) {
+  separator();
+  console.log("You called " + yourFunction.name  );
+  console.log("You passed in " +  yourParams)
+  console.log("  Results --> " + yourFunction(yourParams) )
+  separator();
 }
+
+function separator() {
+  console.log("----------------------------------------------\n");
+}
+
+/*Build a function `my_max()` which takes an array and returns the maximum number. */
+function myMax(list) {
+  var sorted_values = list.sort(function(a, b) { return a - b });
+  return sorted_values[sorted_values.length-1];
+}
+
+var arr = [8, 10, 1, 2, 200, 5, 12];
+printResults(myMax, arr);
 
 
 /*Build a function vowelCount which takes a string and returns the number of vowels it contains */
-
 function vowelCount(str) {
   var result = 0;
   for(i = 0; i < str.length; i++) {
@@ -18,8 +32,15 @@ function vowelCount(str) {
   return result;
 }
 
-/*Build a function reverse() which takes a string and returns all the characters in the opposite position, e.g. reverse("this is a string") // "gnirts a si siht" */
+str = "abcdefghijklmnopqrstuvwxyz";
+printResults(vowelCount, str);
 
+
+/*Build a function reverse() which takes a string and returns all the characters in the opposite position, e.g. reverse("this is a string") // "gnirts a si siht" */
 function reverse(str) {
-  str.split('').reverse().join('');
+  return str.split('').reverse().join('');
 }
+
+myStr = "There once was a man from Mississippi.";
+printResults(reverse, myStr);
+
