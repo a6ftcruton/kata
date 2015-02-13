ecf5eb03a6d21693d091edaed9cddaf9f5cb0865
Jim Sutton <jimsuttonjimsutton@gmail.com>
2015-02-13 09:34:38 -0700

    Add benchmarking. Compare iterative vs recursive approach.

---
+++ binary_chop/benchmark.rb
@@ -0,0 +1,23 @@
+require 'benchmark/ips'
+require './binary_chop'
+require '../binary_chop_recursive/binary_chop_recursive'
+
+include BinaryChopRecursive
+
+Benchmark.ips do |r|
+  r.config(time: 5, warmup: 1)
+  test_array  = Array(1..1000)
+
+  r.report 'binary_chop' do |times|
+    BinaryChop.new(32, test_array)
+    BinaryChop.new(799, test_array)
+  end
+
+  r.report 'binary_chop_recursive' do |times|
+    chop(32, test_array)
+    chop(799, test_array)
+  end
+
+  r.compare!
+
+end
