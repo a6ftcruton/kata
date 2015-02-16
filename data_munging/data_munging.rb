b9da86e5ddfc7c24f423111e25b38611f2b5d58b
Jim Sutton <jimsuttonjimsutton@gmail.com>
2015-02-16 09:14:45 -0700

    Refactor into SRP methods

--- data_munging/data_munging.rb
+++ data_munging/data_munging.rb
@@ -1,13 +1,47 @@
-class ParseWeatherData
-  file = File.open('weather.dat', "r")
-  lines = file.each_line.map(&:split)
+#  file = File.open('weather.dat', "r")
+#  lines = file.each_line.map(&:split)
+#
+#  hash = Hash.new
+#  lines.map do |l|
+#    hash[l[0]] = (l[1].to_f - l[2].to_f)  
+#  end
+#
+#  valid_temps = hash.select { |k,v| v > 0}
+#  smallest_temp_difference = valid_temps.min_by { |k,v| v if v != nil }
+#  puts smallest_temp_difference.last 
 
-  hash = Hash.new
-  lines.map do |l|
-    hash[l[0]] = (l[1].to_f - l[2].to_f)  
+
+class ParseWeatherData
+  attr_reader :file
+
+  def initialize(file)
+    @file = File.open(file, "r")
   end
 
-  valid_temps = hash.select { |k,v| v > 0}
-  smallest_temp_difference = valid_temps.min_by { |k,v| v if v != nil }
-  puts smallest_temp_difference.last 
+  def file_rows
+    file.each_line.map(&:split)
+  end
+
+  def min_temp_difference
+    hash = Hash.new
+    file_rows.map do |l| 
+        hash[l[0]] = (l[1].to_f - l[2].to_f) 
+    end
+    hash
+  end
+  
+  def valid_temps
+    min_temp_difference.select { |k,v| v > 0 }
+  end
+  
+  def smallest_temp_difference
+    valid_temps.min_by { |k,v| v }
+  end
+
+  def day_of_smallest_temp_difference
+    "Smallest temperature difference occured on day " + smallest_temp_difference.first 
+  end
 end
+
+weather = ParseWeatherData.new('weather.dat')
+puts weather.day_of_smallest_temp_difference.inspect
