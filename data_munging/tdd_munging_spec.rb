afa81fb3c97d6cbd43f6088f8ff0052dd2ad32cc
Jim Sutton <jimsuttonjimsutton@gmail.com>
2015-02-17 09:41:01 -0700

    Refactor repetitive test setup into private method

--- data_munging/tdd_munging_spec.rb
+++ data_munging/tdd_munging_spec.rb
@@ -1,33 +1,34 @@
 require './tdd_munging'
 
 describe 'tdd_munging' do
-# Test Data:
+
+#   Test Data:
 #
-# Day Max  Min   Avg
-# 1  88    59    74
-# 2  79    63    71
+#   Day Max  Min   Avg
+#    1  88    59    74
+#    2  79    63    71
   
   it 'parses a .dat file into rows' do
-    file = "./test_data.dat"
-    data = TddMunging.new(file)
-
-    expect(data.rows.count).to eq 3 
+    expect(file.rows.count).to eq 3 
   end
 
   it 'calculates temp diff for each row' do
-    file = "./test_data.dat"
-    data = TddMunging.new(file)
-    diffs = data.calculate_temp_diff
+    diffs = file.calculate_temp_diff
 
     expect(diffs["2"]).to eq 16 
   end
 
   it 'returns the smallest temp diff' do
-    file = "./test_data.dat"
-    data = TddMunging.new(file)
-    diffs = data.calculate_temp_diff
-    smallest = data.day_of_smallest_temp_range(diffs)
+    diffs    = file.calculate_temp_diff
+    smallest = file.day_of_smallest_temp_range(diffs)
 
     expect(smallest).to eq "2"
+  end
+
+  private
+
+  def file
+    filepath = "./test_data.dat"
+    TddMunging.new(filepath)
   end
 end
